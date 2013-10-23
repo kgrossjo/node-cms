b5f42963a8f4830d880c57a659a8f52b4800e4f2
Kai Grossjohann <kai.grossjohann@epochtimes.de>
2013-10-23 23:38:17 +0200

    Highlighting, syntax fixes.

--- routes/article.js
+++ routes/article.js
@@ -6,6 +6,7 @@ exports.index = function(req, res) {
 	    title: 'Article List',
 	    error: err,
 	    articles: result,
+            currenttab: '/articles',
 	});
     });
 }
@@ -17,21 +18,22 @@ exports.edit = function(req, res) {
             title: 'Edit Article',
             error: err,
             article: article,
+            currenttab: (id ? null : '/new'),
         });
     });
 }
 
 exports.save = function(req, res) {
-    var id = req.body.id;
-    var title = req.body.title;
-    var body = req.body.body;
+    var article_id = req.body.id;
+    var article_title = req.body.title;
+    var article_body = req.body.body;
     var article = {
-	id: id,
-	title: title,
-	body: body,
+	id: article_id,
+	title: article_title,
+	body: article_body,
     };
-    db.saveArticle(article, function (err) {
-        res.redirect("/edit/" + id);
+    db.saveArticle(article, function (err, article) {
+        res.redirect("/edit/" + article.id);
     });
 }
 
