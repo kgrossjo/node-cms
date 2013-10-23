8e5c758b06efb0f6e4892402340b20f1d91f6971
Kai Grossjohann <kai.grossjohann@epochtimes.de>
2013-10-23 23:38:59 +0200

    Add support for using it as a REST service.  Hope we can do the client side, soon.

---
+++ routes/rest.js
@@ -0,0 +1,26 @@
+var db = require('../database');
+
+exports.articles = function(req, res) {
+    db.getArticles(function(err, result) {
+        if (err) res.send({ type: "error", error: err});
+        res.send({ type: "article list", articles: result });
+    });
+};
+
+exports.getArticle = function(req, res) {
+    var id = req.params.id;
+    db.getArticle(id, function(err, article) {
+        if (err) res.send({ type: "error", error: err});
+        res.send(article);
+    });
+};
+
+exports.saveArticle = function(req, res) {
+    console.log("*** " + JSON.stringify(req.body));
+    var inp = req.body;
+    var art = { id: inp.id, title: inp.title, body: inp.body, };
+    db.saveArticle(art, function(err, article) {
+        if (err) res.send({ type: "error", error: err});
+        res.send({ type: "ok", id: article.id });
+    });
+};
